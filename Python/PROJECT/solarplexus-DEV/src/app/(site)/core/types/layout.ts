6501d41199f2ecd549cc23aa5506d3c2513e4749
InformalOne <71822962+InformalOne@users.noreply.github.com>
2025-03-13 16:25:19 +0530

    Delete Python/PROJECT/solarplexus-DEV directory

--- Python/PROJECT/solarplexus-DEV/src/app/(site)/core/types/layout.ts
+++
@@ -1,4 +0,0 @@
-export enum SidebarMode {
-  ChatBotMode,
-  EditorMode
-}
--- Python/PROJECT/solarplexus-DEV/src/core/ui/Divider.tsx
+++
@@ -1,7 +0,0 @@
-function Divider() {
-  return (
-    <hr className={'w-full border border-gray-50 dark:border-dark-800'} />
-  );
-}
-
-export default Divider;
